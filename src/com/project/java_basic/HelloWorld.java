66decb317f2d6cba85da3bec55ace2d530f9d2f7
nittaya30 <nittayasaladaeng31@gmail.com>
2025-06-13 11:58:21 +0700

    update file helloworld and variables

---
+++ src/com/project/java_basic/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.project.java_basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        //คำสั่งในการแสดงผลข้อคาวม
+    }
+}
---
+++ src/com/project/java_basic/TypeVariables.java
@@ -0,0 +1,19 @@
+package com.project.java_basic;
+
+public class TypeVariables {
+    public static void main(String[] args) {
+        String name ="Pei"; // ประกาศตัวแปรแบบข้อความ
+        System.out.println(name);
+
+        int number = 4;
+        System.out.println(number);
+
+        double num = 15.44;
+        char A = 'b';
+        boolean result = false;
+        // Ture แปลว่า เป็นจริง
+        // False แปลว่า เท็จ
+
+        System.out.println(A+num+"Pei");
+    }
+}
